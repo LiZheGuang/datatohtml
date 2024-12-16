094426d705e51414cf2e76e01219633c7485cc04
李哲广 <lizheguang@chaboshi.cn>
2024-12-16 19:40:48 +0800

    code

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+
+import ex2json from "@netjoy/ex2json";
+const FileDom = document.getElementById('zzcodeFile')
+
+console.log(ex2json)
+
+console.log('21321')
+
+function handleFileChange(e){
+    console.log('handleFileChange')
+    console.log(w)
+}
+
